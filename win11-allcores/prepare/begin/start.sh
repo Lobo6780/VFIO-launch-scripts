5f48bb98f5d8ff75e25ff153775bd8ee98b0d5e4
Piotr K <75329810+Lobo6780@users.noreply.github.com>
2024-02-01 19:08:12 +0100

    Update

---
+++ win11-allcores/prepare/begin/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cpupower --cpu all frequency-set --governor performance
---
+++ win11/prepare/begin/hugepages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 3 > /proc/sys/vm/drop_caches
+echo 1 > /proc/sys/vm/compact_memory
+
+HUGEPAGES=8192
+
+echo "Allocating hugepages..."
+echo $HUGEPAGES > /proc/sys/vm/nr_hugepages
+ALLOC_PAGES=`cat /proc/sys/vm/nr_hugepages`
+
+TRIES=0
+while (( $ALLOC_PAGES != $HUGEPAGES && $TRIES < 1000 ))
+do
+  echo 1 > /proc/sys/vm/compact_memory
+  echo $HUGEPAGES > /proc/sys/vm/nr_hugepages
+  ALLOC_PAGES=`cat /proc/sys/vm/nr_hugepages`
+  echo "Tried to allocate hugepages. Got pages $ALLOC_PAGES / $HUGEPAGES"
+  let TRIES+=1
+done
+
+if [ "$ALLOC_PAGES" -ne "$HUGEPAGES" ]
+then
+  echo "Not able to allocate hugepages"
+  echo 0 > /proc/sys/vm/nr_hugepages
+  exit 1
+fi
+
